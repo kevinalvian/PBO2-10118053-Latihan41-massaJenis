e6370e0cc6e359d87398881800f966d57d732174
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-11-07 22:26:48 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118053/latihan41/massajenis/PBO210118053Latihan41MassaJenis.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118053.latihan41.massajenis;
+
+/**
+ *
+ * @author ACER
+ */
+public class PBO210118053Latihan41MassaJenis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        massa hasil = new massa();
+        hasil.setSisi(5);
+        hasil.setMasa(250); 
+        hasil.MassaJenis();
+        
+       
+        hasil.getSisi();
+        hasil.getMasa(); 
+        hasil.volume();
+    }
+    
+}
